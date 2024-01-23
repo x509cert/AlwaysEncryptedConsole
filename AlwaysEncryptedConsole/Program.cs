9309424340a16a51d9ce442dc8c840cd548bc11f
Michael Howard <mikehow@microsoft.com>
2024-01-23 15:21:50 -0600

    Broke code into two source files so it's easier to read

--- AlwaysEncryptedConsole/Program.cs
+++ AlwaysEncryptedConsole/Program.cs
@@ -1,57 +1,40 @@
 ﻿using Azure.Core;
 using Azure.Identity;
 using Microsoft.Data.SqlClient;
-using System.Text;
-using Azure.Security.KeyVault.Secrets;
 
-static string ByteArrayToHexString(byte[] byteArray, int max)
+partial class Program
 {
-    StringBuilder hex = new StringBuilder(byteArray.Length * 2);
-    foreach (byte b in byteArray)
-        hex.AppendFormat("{0:x2}", b); 
-
-    return hex.ToString()[..max];
-}
-
-// Get URI of AKV from env var
-static string? GetAKVConnectionString() 
-    => Environment.GetEnvironmentVariable("AKVContosoHR", EnvironmentVariableTarget.Process);
-
-// Get SQL Connection String from AKV
-static string GetSQLConnectionString(DefaultAzureCredential cred)
-{
-    string? akvConn = GetAKVConnectionString();
-    var client = new SecretClient(new Uri(akvConn), cred);
-    var sqlConn = client.GetSecret("ContosoHRSQLString");
-    return sqlConn.Value.Value;
-}
-
-var credential = new DefaultAzureCredential();
-var oauth2TokenSql = credential.GetToken(
-        new TokenRequestContext(
-            new[] { "https://database.windows.net/.default" })).Token;
-
-var connectionString = GetSQLConnectionString(credential);
-SqlConnection conn = new(connectionString) {
-    AccessToken = oauth2TokenSql
-};
-
-conn.Open();
-
-SqlCommand sqlCommand = new("SELECT * FROM Employees", conn);
-using var sqlDataReader = sqlCommand.ExecuteReader();
-
-while (sqlDataReader.Read())
-{
-    for (int i = 0; i < sqlDataReader.FieldCount; i++)
+    static void Main()
     {
-        var value = sqlDataReader.GetValue(i);
-        var type = sqlDataReader.GetFieldType(i);
+        var credential = new DefaultAzureCredential();
+        var oauth2TokenSql = credential.GetToken(
+                new TokenRequestContext(
+                    new[] { "https://database.windows.net/.default" })).Token;
 
-        if (type == typeof(byte[]))
-            value = ByteArrayToHexString(value as byte[], 8);
+        var connectionString = GetSQLConnectionString(credential, useAE: false);
+        SqlConnection conn = new(connectionString)
+        {
+            AccessToken = oauth2TokenSql
+        };
 
-        Console.Write(value + "\t"); 
+        conn.Open();
+
+        SqlCommand sqlCommand = new("SELECT Top 10 SSN, Salary, LastName, FirstName FROM Employees", conn);
+        using var sqlDataReader = sqlCommand.ExecuteReader();
+
+        while (sqlDataReader.Read())
+        {
+            for (int i = 0; i < sqlDataReader.FieldCount; i++)
+            {
+                var value = sqlDataReader.GetValue(i);
+                var type = sqlDataReader.GetFieldType(i);
+
+                if (type == typeof(byte[]))
+                    value = ByteArrayToHexString(value as byte[], 16);
+
+                Console.Write(value + "\t");
+            }
+            Console.WriteLine();
+        }
     }
-    Console.WriteLine(); 
 }
---
+++ AlwaysEncryptedConsole/Utils.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Identity;
+using System.Text;
+using Azure.Security.KeyVault.Secrets;
+
+partial class Program
+{
+    public static string ByteArrayToHexString(byte[] byteArray, int max)
+    {
+        StringBuilder hex = new StringBuilder(byteArray.Length * 2);
+        foreach (byte b in byteArray)
+            hex.AppendFormat("{0:x2}", b);
+
+        return hex.ToString()[..max];
+    }
+
+    // Get URI of AKV from env var
+    public static string? GetAKVConnectionString()
+        => Environment.GetEnvironmentVariable("AKVContosoHR", EnvironmentVariableTarget.Process);
+
+    // Get SQL Connection String from AKV
+    public static string GetSQLConnectionString(DefaultAzureCredential cred, bool useAE = true)
+    {
+        string? akvConn = GetAKVConnectionString();
+        var client = new SecretClient(new Uri(akvConn), cred);
+        var sqlConn = client.GetSecret("ContosoHRSQLString");
+        var sql = sqlConn.Value.Value;
+
+        if (useAE)
+            sql = sql + "Column Encryption Setting=Enabled";
+
+        return sql;
+    }
+}
