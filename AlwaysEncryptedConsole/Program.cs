ef2399894d7de2aa8a19cca7aead6a5486939d47
Michael Howard <mikehow@microsoft.com>
2024-01-24 12:30:42 -0600

    Made it easier to flip between AE and not AE, added (c) text

--- AlwaysEncryptedConsole/Program.cs
+++ AlwaysEncryptedConsole/Program.cs
@@ -1,4 +1,16 @@
-﻿using Azure.Core;
+﻿//********************************************************* 
+// 
+// Copyright (c) Microsoft. All rights reserved. 
+// This code is licensed under the MIT License (MIT). 
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF 
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY 
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR 
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT. 
+// 
+// Author: Michael Howard, Azure Data Security
+//*********************************************************
+
+using Azure.Core;
 using Azure.Identity;
 using Microsoft.Data.SqlClient;
 using System.Data;
@@ -7,34 +19,54 @@ partial class Program
 {
     static void Main()
     {
+        bool useAlwaysEncrypted = false;
+
         // login to Azure
         var credential = new DefaultAzureCredential();
         var oauth2TokenSql = credential.GetToken(
                 new TokenRequestContext(
-                    new[] { "https://database.windows.net/.default" })).Token;
+                    ["https://database.windows.net/.default"])).Token;
 
         // login to Azure SQL
-        var connectionString = GetSQLConnectionString(credential, useAE: true);
+        var connectionString = GetSQLConnectionString(credential, useAE: useAlwaysEncrypted);
         SqlConnection conn = new(connectionString) {
             AccessToken = oauth2TokenSql
         };
-
         conn.Open();
 
-        // register AKV for AE use by this code
-        RegisterAkvForAe(credential);
+        // from here on is real database work
+        SqlCommand sqlCommand;
 
-        // Perform a query
-        string query =
-            "SELECT Top 10 SSN, Salary, LastName, FirstName FROM Employees WHERE Salary > @MinSalary or SSN LIKE @SSN";
-        
-        SqlCommand sqlCommand = new(query, conn);
+        if (useAlwaysEncrypted == false)
+        {
+            string query =
+                "SELECT Top 10 SSN, Salary, LastName, FirstName " +
+                "FROM Employees";
 
-        // set the minimum salary
-        // we MUST use parameters
-        SqlParameter minSalaryParam = new("@MinSalary", SqlDbType.Money);
-        minSalaryParam.Value = 50000;
-        sqlCommand.Parameters.Add(minSalaryParam);
+            sqlCommand = new(query, conn);
+        }
+        else
+        {
+            // register AKV for AE use by this code
+            RegisterAkvForAe(credential);
+
+            // query to find minimum salary with specifical SSN
+            string query =
+                "SELECT Top 10 SSN, Salary, LastName, FirstName " +
+                "FROM Employees WHERE Salary > @MinSalary AND SSN LIKE @SSN " +
+                "ORDER by Salary DESC";
+
+            sqlCommand = new(query, conn);
+
+            // we MUST use parameters
+            SqlParameter minSalaryParam = new("@MinSalary", SqlDbType.Money);
+            minSalaryParam.Value = 50000;
+            sqlCommand.Parameters.Add(minSalaryParam);
+
+            SqlParameter ssnParam = new("@SSN", SqlDbType.Char);
+            ssnParam.Value = "6%";
+            sqlCommand.Parameters.Add(ssnParam);
+        }
 
         // now read the data
         using var sqlDataReader = sqlCommand.ExecuteReader();
@@ -45,6 +77,7 @@ partial class Program
                 var value = sqlDataReader.GetValue(i);
                 var type = sqlDataReader.GetFieldType(i);
 
+                // if the data is a byte array (ie; ciphertext) dump the hex string
                 if (type == typeof(byte[]))
                     value = ByteArrayToHexString(value as byte[], 16);
 
--- AlwaysEncryptedConsole/Utils.cs
+++ AlwaysEncryptedConsole/Utils.cs
@@ -1,4 +1,16 @@
-﻿using Azure.Identity;
+﻿//********************************************************* 
+// 
+// Copyright (c) Microsoft. All rights reserved. 
+// This code is licensed under the MIT License (MIT). 
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF 
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY 
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR 
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT. 
+// 
+// Author: Michael Howard, Azure Data Security
+//*********************************************************
+
+using Azure.Identity;
 using System.Text;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.Data.SqlClient;
@@ -7,6 +19,7 @@ using Microsoft.Data.SqlClient.AlwaysEncrypted.AzureKeyVaultProvider;
 partial class Program
 {
     // Helper function to dump binary data
+    // can truncate the output if needed
     public static string ByteArrayToHexString(byte[] byteArray, int max)
     {
         StringBuilder hex = new StringBuilder(byteArray.Length * 2);
